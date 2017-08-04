df7bb03a1138b56004022b83e28911da6b36e160
Jeff <jeffschultz.js@gmail.com>
2017-08-04 12:01:21 -0700

    forked my branch, working on models & associations

---
+++ models/users.js
@@ -0,0 +1,23 @@
+module.exports = function(sequelize, DataTypes) {
+  var Users = sequelize.define("Users", {
+    // Giving the Users model a name, and userName
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    userName: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+  });
+
+  Users.associate = function(models) {
+    // Associating User with MovieTitles
+    // When an Author is deleted, also delete any associated MovieTitles
+    Users.hasMany(models.MovieTitles, {
+      onDelete: "cascade"
+    });
+  };
+
+  return Users;
+};
